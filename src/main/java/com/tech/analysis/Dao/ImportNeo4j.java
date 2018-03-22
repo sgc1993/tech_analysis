d7ee650676385320d231e49f593b9bbf06eacabd
lhxcy <lh_xcy@163.com>
2018-03-22 17:00:33 +0800

    3.22  17:00

---
+++ src/main/java/com/tech/analysis/Dao/ImportNeo4j.java
@@ -0,0 +1,7 @@
+package com.tech.analysis.Dao;
+
+/**
+ * 该类的主要功能是向neo4j中插入数据
+ */
+public class ImportNeo4j {
+}
---
+++ src/main/java/com/tech/analysis/Dao/test.java
@@ -0,0 +1,15 @@
+package com.tech.analysis.Dao;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+//import org
+public class test {
+    public static void main(String[] args){
+        System.out.println("test");
+        KeywordsDao keywordsDao = new KeywordsDao();
+        HashMap<String,ArrayList<Integer>> data = keywordsDao.getData("高精度");
+        String str =keywordsDao.getJsonString(keywordsDao.getCommunity(data));
+        System.out.println(str);
+    }
+}
