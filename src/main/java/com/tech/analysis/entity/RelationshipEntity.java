dea6ec91d30f9fb58abda1fa8810eb8ac6b7756d
lhxcy <lh_xcy@163.com>
2018-03-26 11:42:28 +0800

    xcy

---
+++ src/main/java/com/tech/analysis/entity/RelationshipEntity.java
@@ -0,0 +1,51 @@
+package com.tech.analysis.entity;
+import java.io.Serializable;
+/**
+ * Created by XCY on 2018/3/26.
+ */
+public class RelationshipEntity implements Serializable {
+    private static final long serialVersionUID = 1269373329410167403l;
+    private Long source;
+    private Long target;
+    private Long times;
+    private String type;
+
+    public RelationshipEntity(Long source, Long target, Long times, String type) {
+        this.source = source;
+        this.target = target;
+        this.times = times;
+        this.type = type;
+    }
+
+    public Long getSource() {
+        return source;
+    }
+
+    public void setSource(Long source) {
+        this.source = source;
+    }
+
+    public Long getTarget() {
+        return target;
+    }
+
+    public void setTarget(Long target) {
+        this.target = target;
+    }
+
+    public Long getTimes() {
+        return times;
+    }
+
+    public void setTimes(Long times) {
+        this.times = times;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
